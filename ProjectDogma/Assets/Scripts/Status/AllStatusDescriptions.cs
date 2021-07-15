3b14f7cd722451ab20aa6da5373a81dc400f26e6
Evan1551 <1085986321@qq.com>
2021-07-15 22:11:57 +0800

    Create some scripts about status

---
+++ ProjectDogma/Assets/Scripts/Status/AllStatusDescriptions.cs
@@ -0,0 +1,41 @@
+
+//这个脚本包含了战斗中所有可见单位的状态
+
+/// <summary>
+/// 战斗地图网格单位状态
+/// </summary>
+public enum BattleGridStatus
+{
+    Normal,//通常
+    Burning,//燃烧
+    Obstacle,//障碍物
+    Flag,//旗帜所在地
+    //有待补充
+}
+
+
+/// <summary>
+/// 敌人行为意图
+/// </summary>
+public enum EnemyBehavioralIntention
+{
+    Unknown,//未知
+    Attack,//攻击
+    Defense,//防御（给予护盾）
+    AttackAndDenfense,//攻守兼备
+    AccumulatePower,//蓄力
+    //有待补充
+}
+
+
+/// <summary>
+/// 角色状态
+/// </summary>
+public enum CharacterStatus
+{
+    BeInspired,//鼓舞
+    ShieldProtection,//护盾保护
+
+    Poisoning,//中毒
+    Burning,//燃烧
+}
---
+++ ProjectDogma/Assets/Scripts/Status/StatusController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// ×´Ì¬×ª±ä
+    /// </summary>
+    /// <param name="target"></param>
+    public void ChangeStatus(Object target)
+    {
+
+    }
+}
